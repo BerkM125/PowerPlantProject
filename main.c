acb081ab7505563a0761e480aeca8924875037d2
BerkM125 <49427416+BerkM125@users.noreply.github.com>
2019-10-20 19:46:56 -0700

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 
 #define BUDGET 2000
 #define REQUIRED_POWER 1500
-#define MAXIMUM_ENVIRONMENTAL_IMPACT 500
+#define MAXIMUM_ENVIRONMENTAL_IMPACT 299
 
 typedef struct powerplant_struct {
 	float cost;
@@ -27,19 +27,20 @@ powerplant *init_powerplant (powerplant *p, float cost, int capacity, int enviro
 	return(p);
 }
 
-void calculate_totals (int windcount, int hydrocount, int solarcount, int coalcount, int nuclearcount) {
+plant_totals calculate_totals (int windcount, int hydrocount, int solarcount, int coalcount, int nuclearcount) {
 	plant_totals wind;
 	plant_totals hydro;
 	plant_totals solar;
 	plant_totals coal;
 	plant_totals nuclear;
+	plant_totals totalstruct;
 	float totalcost;
 	int totalcapacity;
 	int totalei;
 
 	if(windcount > WINDFARM_MAXIMUM || hydrocount > HYDROPLANT_MAXIMUM || solarcount > SOLARPOWER_MAXIMUM) {
 		printf("\nPOWERPLANT COUNT EXCEEDS LIMIT.\n");
-		return;
+		return totalstruct;
 	}
 
 	wind.capacity = (windfarm.capacity*windcount); 
@@ -75,18 +76,21 @@ void calculate_totals (int windcount, int hydrocount, int solarcount, int coalco
 
 	if(totalcost > BUDGET) printf("\n\nCOST EXCEEDS LIMIT\n");
 	if(totalcapacity < REQUIRED_POWER) printf("\n\nINSUFFICIENT POWER\n");
-	if(totalei > MAXIMUM_ENVIRONMENTAL_IMPACT) printf("\n\nYOU ARE LITERALLY DESTROYING THE WORLD\n");
-	return;
+	totalstruct.capacity = totalcapacity;
+	totalstruct.cost = totalcost;
+	totalstruct.environmental_impact = totalei;
+	return totalstruct;
 }
 
 int main (void) {
 	int windnum, hydronum, solarnum, coalnum, nuclearnum;
+	plant_totals totals;
 	init_powerplant (&windfarm, WINDFARM_COST, WINDFARM_CAPACITY, WINDFARM_EI);
 	init_powerplant (&hydroplant, HYDROPLANT_COST, HYDROPLANT_CAPACITY, HYDROPLANT_EI);
 	init_powerplant (&solarenergy, SOLARPOWER_COST, SOLARPOWER_CAPACITY, SOLARPOWER_EI);
 	init_powerplant (&coalplant, COAL_COST, COAL_CAPACITY, COAL_EI);
 	init_powerplant (&nuclearplant, NUCLEAR_COST, NUCLEAR_CAPACITY, NUCLEAR_EI);
-	while(1) {
+	/*while(1) {
 		printf("Wind Plant Count: ");
 		scanf("%d", &windnum);
 		printf("Hydro Plant Count: ");
@@ -98,6 +102,15 @@ int main (void) {
 		printf("Nuclear Plant Count: ");
 		scanf("%d", &nuclearnum);
 		calculate_totals (windnum, hydronum, solarnum, coalnum, nuclearnum);
+	}*/
+	while (totals.environmental_impact > MAXIMUM_ENVIRONMENTAL_IMPACT || totals.capacity < REQUIRED_POWER || totals.cost > BUDGET) {
+		windnum = rand() % WINDFARM_MAXIMUM+1;
+		hydronum = rand() % HYDROPLANT_MAXIMUM+1;
+		solarnum = rand() % SOLARPOWER_MAXIMUM+1;
+		nuclearnum = rand() % 15;
+		coalnum = 0;
+		printf("\nWind Farms: %d\nHydro Plants: %d\nSolar Plants: %d\nNuclear Plants: %d\nCoal Factories: %d\n", windnum, hydronum, solarnum, nuclearnum, coalnum);
+		totals = calculate_totals (windnum, hydronum, solarnum, coalnum, nuclearnum);
 	}
 	return (0);
 }
